f729b4c0fa5c7ae268b3957f2f34615f71b73677
AnicetoB <aniceto.bokari.egea@gmail.com>
2014-10-08 18:58:19 +0200

    Avances varios - última hora Miércoles

---
+++ Assets/Scripts/entrarPuerta1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class entrarPuerta1 : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Iniciar ();
+		Application.LoadLevel ("Nivel_2");
+		Debug.Log ("¡Enhorabuena, pasas al Nivel 2!");
+	}
+}
---
+++ Assets/Scripts/gameScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) //KeyCode es codigo ASCII, de modo que funcionara en todos los teclados a diferencia de "cualquiertecla"
+			Reiniciar();
+		}
+
+	public void Salir(){
+		Application.Quit (); //Aplication sirve para cosas generales del juego/aplicaciona
+	}
+
+	public void Reiniciar(){
+		Application.LoadLevel ("Menu_principal");
+	}
+
+	public void Iniciar() {
+		Application.LoadLevel ("Nivel_1");
+	}
+}
