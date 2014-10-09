9972711960a5cd466f3371689d6252d28f2ad6dd
AnicetoB <aniceto.bokari.egea@gmail.com>
2014-10-09 19:25:36 +0200

    Avaces jueves 9

--- Assets/Scripts/entrarPuerta1.cs
+++ Assets/Scripts/entrarPuerta1.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class entrarPuerta1 : MonoBehaviour {
-
+	public string nivel = "nivel1";
 	void OnTriggerEnter2D(Collider2D other){
-		//Iniciar ();
-		Application.LoadLevel ("Nivel_2");
-		Debug.Log ("¡Enhorabuena, pasas al Nivel 2!");
+		if (other.transform.tag == "Player") {
+		Application.LoadLevel (nivel);
+		Debug.Log ("¡Enhorabuena, pasas al siguiente nivel!");
+		}
 	}
 }
